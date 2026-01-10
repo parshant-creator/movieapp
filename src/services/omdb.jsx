f5b7496144f685ba982a2c84e6ceeef358683282
Prashant <prashantrajpuria@gmail.com>
2026-01-10 22:40:36 +0530

    Revert to OMDB API with CORS mode for better compatibility

--- src/services/omdb.jsx
+++ src/services/omdb.jsx
@@ -1,16 +1,11 @@
 export const searchMovies = async (query, page = 1) => {
-  const API_KEY = "YOUR_TMDB_API_KEY" // Get from https://www.themoviedb.org/settings/api
   const res = await fetch(
-    `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}&page=${page}`
+    `https://www.omdbapi.com/?apikey=5761eeda&s=${query}&page=${page}`,
+    { mode: 'cors' }
   )
   const data = await res.json()
   return {
-    movies: data.results.map(movie => ({
-      Title: movie.title,
-      Year: movie.release_date ? movie.release_date.split('-')[0] : 'N/A',
-      imdbID: movie.id.toString(), // TMDB ID as string
-      Poster: movie.poster_path ? `https://image.tmdb.org/t/p/w300${movie.poster_path}` : 'N/A'
-    })),
-    totalResults: data.total_results
+    movies: data.Search || [],
+    totalResults: Number(data.totalResults || 0),
   }
 }
